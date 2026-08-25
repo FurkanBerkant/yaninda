794ded877a13f7f4ce48183175ae6053a641a3c7
berkant <76666191+FurkanBerkant@users.noreply.github.com>
2026-08-25 14:57:50 +0300

    chore: bump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.berkant.yaninda"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
